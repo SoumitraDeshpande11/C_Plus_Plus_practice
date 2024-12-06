0fc0b19ae5c8a6209190408ed709891cf334ac72
Soumitra <soumitradesh11@gmail.com>
2024-12-06 10:58:34 +0530

    Add files via upload

---
+++ b.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    char str1[10] = "I am";
+    char str2[10] = " watching";
+    char str3[10] = " you";
+    char str4[10] = " nigga";
+    cout<<str1<<str2<<str3<<str4<<endl;
+    cout<< "this is the string before";
+
+    cout << "\n the first string is : ";
+    cin>> str1;
+    cout << "the second string is : ";
+    cin >> str2;
+    cout << "the third string is : ";
+    cin >> str3;
+    cout << "the fourth string is : ";
+    cin >> str4;
+    cout<<str1<<" "<<str2<<" "<<str3<<" " <<str4;
+
+
+
+
+
+}
